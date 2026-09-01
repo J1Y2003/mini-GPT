060ab45468e1849e60232e8577390fad84d0a3a7
J1Y2003 <j1y@Jaewons-MacBook-Pro.local>
2026-09-02 01:10:17 +0900

    Added Multi-Head Attention

--- mini_gpt.py
+++ mini_gpt.py
@@ -1,12 +1,13 @@
 import torch
+import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
 batch_size = 32
 block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
 device = "mps"
 eval_iters = 200
 n_embd = 32
@@ -54,12 +55,43 @@ def estimate_loss():
     model.train()
     return out
 
-class BigramLanguageModel(nn.Module):
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
 
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+
+        out = wei @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
+class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -68,6 +100,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B, T, C)
+        x = self.sa_heads(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
@@ -83,13 +116,17 @@ class BigramLanguageModel(nn.Module):
 
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
+
+            idx_cond = idx[:, -block_size:]
+
+            logits, loss = self(idx_cond)
             logits = logits[:, -1, :] # becomes (B, C) we pick the last word, this is what we predicted
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
 
         return idx
+
 
 model = BigramLanguageModel()
 m = model.to(device)

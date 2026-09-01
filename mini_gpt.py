00bc01d1ae9344bee74562ab210dea99c4cea177
J1Y2003 <j1y@Jaewons-MacBook-Pro.local>
2026-09-02 01:20:00 +0900

    Skip connections

--- mini_gpt.py
+++ mini_gpt.py
@@ -95,6 +95,7 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embd, n_embd),
             nn.ReLU(),
+            nn.Linear(n_embd, n_embd),
         )
 
     def forward(self, x):

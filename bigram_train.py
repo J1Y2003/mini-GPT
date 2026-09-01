5fb5305aa342b18cd109e6d848b4b701581261e3
J1Y2003 <j1y@Jaewons-MacBook-Pro.local>
2026-09-02 00:38:21 +0900

    Clean up code

--- bigram_train.py
+++ bigram_train.py
@@ -9,6 +9,7 @@ eval_interval = 300
 learning_rate = 1e-2
 device = "mps"
 eval_iters = 200
+n_embd = 32
 
 torch.manual_seed(1337)
 
@@ -55,12 +56,19 @@ def estimate_loss():
 
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
-        logits = self.token_embedding_table(idx)
+        B, T = idx.shape
+
+        tok_emb = self.token_embedding_table(idx) # (B, T, C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B, T, C)
+        logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
             loss = None
@@ -83,7 +91,7 @@ class BigramLanguageModel(nn.Module):
 
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
